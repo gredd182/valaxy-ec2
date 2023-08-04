bd485db839493911c3836d29ace76defa974e7af
gredd182 <134130676+gredd182@users.noreply.github.com>
2023-08-04 15:43:21 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,12 @@
-
+provider "aws" {
+  region = "eu-west-1"
+}
+resource "aws_s3_bucket" "example" {
+  bucket = "statefile12345678"
+  acl = "private"
+  versioning {
+    enabled = true
+  }tags = {
+    Environment = "test"
+  }
+}
